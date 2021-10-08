fdd53d8438c09542b3ff03ddeeac59cd83b8b78a
Subhajit Sahu <wolfram77@gmail.com>
2021-10-08 12:43:01 +0530

    :skunk: w/ kaggle py

---
+++ main.py
@@ -0,0 +1,18 @@
+# https://www.kaggle.com/wolfram77/puzzlef-sum-cuda-memcpy-vs-inplace
+import os
+from IPython.display import FileLink
+src="sum-cuda-memcpy-vs-inplace"
+out="{}.txt".format(src)
+!printf "" > "$out"
+display(FileLink(out))
+!ulimit -s unlimited && echo ""
+!nvidia-smi && echo ""
+
+# Download program
+!rm -rf $src
+!git clone https://github.com/puzzlef/$src
+!echo ""
+
+# Run
+!nvcc -std=c++17 -Xcompiler -O3 $src/main.cu
+!stdbuf --output=L ./a.out 2>&1 | tee -a "$out"
